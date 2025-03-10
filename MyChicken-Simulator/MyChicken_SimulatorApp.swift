3e4c6c399ac57a89626b8f41a7df41e56ddb0e2a
nwent-dev <nwent@proton.me>
2025-03-10 09:49:00 +0300

    Initial Commit

---
+++ MyChicken-Simulator/MyChicken_SimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyChicken_SimulatorApp.swift
+//  MyChicken-Simulator
+//
+//  Created by Александр Калашников on 10.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyChicken_SimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
